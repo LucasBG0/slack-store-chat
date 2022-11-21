746e979a85afcef62c0448d5f249f03256bf2c63
Weslley de Campos <weslleydrum@hotmail.com>
2022-11-20 21:11:01 -0300

    wip: creating a function to get thread is triggered to get message and save

--- app.py
+++ app.py
@@ -13,7 +13,18 @@ app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 def message_hello(ack, shortcut, client):
     ack()
     print(shortcut)
-
+    if 'thread_ts' in shortcut['message']:
+         #buscar a mensagem e verificar sehá threads
+        slack_response = client.conversations_replies(
+            channel=shortcut['channel']['id'],
+            ts=shortcut['message']['thread_ts']
+        )
+        # print(slack_response)
+        # print('\n')
 # Start your app
 if __name__ == "__main__":
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
+
+
+
+channel_id, thread_ts
